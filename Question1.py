783a77335cc5f8e76bee3b1336f1724deacd9566
sahesers <164067957+sahesers@users.noreply.github.com>
2024-03-22 00:32:36 +0300

    Delete Question1.py

--- Question1.py
+++
@@ -1,6 +0,0 @@
-#1-Kullanıcının girdiği boy ve ağırlık değerlerine göre vücut kitle indeksini (VKİ = ağırlık/(boy*boy)) hesaplayınız.
-
-boy  = float(input('Lütfen boyunuzu "metre" cinsinden giriniz...'))
-kilo = float(input('Lütfen kilonuzu "kilogram" cinsinden giriniz...'))
-VKİ= kilo / (boy * boy)
-print("Vücut Kitle Endeksiniz", VKİ)
