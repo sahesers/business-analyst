a9a3ec840b8afa0c0d882597be2b3fce3a457859
sahesers <164067957+sahesers@users.noreply.github.com>
2024-03-22 00:34:03 +0300

    Delete Question5.py

--- Question5.py
+++
@@ -1,8 +0,0 @@
-#Kullanıcıdan alınan bir sayının palindrom olup olmadığını bulan bir program yazınız.
-
-
-for i in range(100,1000):
-    s = str(i)
-    t = s[::-1]
-    if s == t:
-        print(s)
