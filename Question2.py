8a95b40005bd46d75da4352ae00948892f9ef67c
sahesers <164067957+sahesers@users.noreply.github.com>
2024-03-22 00:32:48 +0300

    Delete Question2.py

--- Question2.py
+++
@@ -1,7 +0,0 @@
-#2-Maaşı ve zam oranı girilen işçinin zamlı maaşını hesaplayarak ekranda gösteriniz.
-
-Güncelmaas=0
-maas=input("Mevcut Maasi Gir : ")
-zam=input("Zam Oranı(%) : ")
-Güncelmaas=int(maas)+(int(maas)*int(zam)/100)
-print("Zamli Maaş :",Güncelmaas)
