cfb15b17b5276a6cf3498bcde85455b521318b3d
sahesers <164067957+sahesers@users.noreply.github.com>
2024-03-22 00:33:11 +0300

    Delete Question4.py

--- Question4.py
+++
@@ -1,7 +0,0 @@
-# 4-Dairenin alanını ve çevresini hesaplayan python kodunu yazınız.(Dairenin yarıçapını kullanıcıdan alınız)
-
-yariçap= input(str("Lütfen dairenin yariçapini giriniz: "))
-Çevre = 2 * 3 * float (yariçap)
-alan= 3 * float (yariçap) * float (yariçap)
-print("Dairenin Alanı", alan)
-print("Dairenin Çevresi", Çevre)
